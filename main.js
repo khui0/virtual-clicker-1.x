3761d65d9cef5999bb4b99c9dc919618b2259e40
Kenny <101839505+khui0@users.noreply.github.com>
2022-09-26 22:39:13 -0400

    Submitting clicks working

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+const questionInput = document.getElementById("question");
+const answerInput = document.getElementById("answer");
+
+var code = localStorage.getItem("code") || "";
+var history = JSON.parse(localStorage.getItem("history") || "[]");
 document.body.className = localStorage.getItem("theme") || "";
+
+// Show enter code modal if no saved seat code is found
+if (!code) {
+    document.getElementById("code").showModal()
+}
 
 document.querySelectorAll("[data-theme]").forEach(item => {
     item.addEventListener("click", () => {
@@ -12,8 +24,11 @@ document.querySelectorAll("[data-open]").forEach(item => {
     item.addEventListener("click", () => {
         let id = item.getAttribute("data-open");
         document.getElementById(id).showModal();
-        if (id != "task") {
+        if (id != "code") {
             document.getElementById(id).querySelector("button[data-close]").focus();
+        }
+        else {
+            document.getElementById("seat-code").value = code;
         }
     });
 });
@@ -22,6 +37,51 @@ document.querySelectorAll("[data-close]").forEach(item => {
     item.addEventListener("click", () => {
         document.getElementById(item.getAttribute("data-close")).close();
     });
+});
+
+document.getElementById("seat-code").addEventListener("input", e => {
+    if (parseInt(e.target.value)) {
+        e.target.value = parseInt(e.target.value);
+    }
+    else {
+        e.target.value = "";
+    }
+});
+
+document.getElementById("save-code").addEventListener("click", () => {
+    let input = document.getElementById("seat-code").value;
+    let array = input.split("");
+    // Only allows codes that are possible in room 233
+    if (array[0] > 0 && array[1] > 0 && array[2] > 0 && array[0] <= 9 && array[1] <= 6 && array[2] <= 5) {
+        code = input;
+        localStorage.setItem("code", code);
+        document.getElementById("code").close();
+    }
+    else {
+        alert("That seat code isn't possible... 🤔");
+    }
+});
+
+document.getElementById("submit").addEventListener("click", () => {
+    let question = questionInput.value;
+    let answer = answerInput.value;
+    if (code) {
+        if (question?.trim() && answer?.trim()) {
+            submitClick(999, question, answer);
+            questionInput.value = "";
+            answerInput.value = "";
+        }
+        else {
+            alert("Question and answer can't be empty 😔");
+        }
+    }
+    else {
+        document.getElementById("code").showModal();
+    }
+});
+
+document.getElementById("reset-history").addEventListener("click", () => {
+    localStorage.removeItem("history");
 });
 
 document.getElementById("reset-theme").addEventListener("click", () => {
@@ -33,3 +93,21 @@ document.getElementById("reset").addEventListener("click", () => {
         localStorage.clear();
     }
 });
+
+function submitClick(code, question, answer) {
+    let fields = {
+        "entry.1896388126": code,
+        "entry.1232458460": question,
+        "entry.1065046570": answer
+    }
+    let params = new URLSearchParams(fields).toString();
+    let url = "https://docs.google.com/forms/d/e/1FAIpQLSfwDCxVqO2GuB4jhk9iAl7lzoA2TsRlX6hz052XkEHbLrbryg/formResponse?" + params;
+
+    fetch(url, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    });
+}
